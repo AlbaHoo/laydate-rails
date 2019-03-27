353598a94ee730cc88356e1c9966d79acdf4f3d7
Alba Hoo <alba@vivi.io>
2019-03-28 00:37:15 +1100

    Initial commit

---
+++ lib/laydate/rails.rb
@@ -0,0 +1,7 @@
+require "laydate/rails/version"
+
+module Laydate
+  module Rails
+    class Engine < ::Rails::Engine;end
+  end
+end
---
+++ lib/laydate/rails/version.rb
@@ -0,0 +1,5 @@
+module Laydate
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
